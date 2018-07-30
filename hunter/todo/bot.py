26cc518e7a8c16eaf4b54db433d9be5b74d566f1
Justin Sobieski <jmksobieski@gmail.com>
2018-07-30 10:16:40 -0400

    Update bot.py

--- hunter/todo/bot.py
+++ hunter/todo/bot.py
@@ -6,7 +6,7 @@ config = pyson.Pyson('tasks.json')
 tasks = config.data.get('tasklist')
 token = 'token'
 
-bot = commands.Bot(command_prefix='~')
+bot = commands.Bot(command_prefix='!')
 
 
 @bot.event
@@ -24,8 +24,14 @@ async def connect():
             await asyncio.sleep(5)
 
 
-@bot.command(pass_context=True, hidden=True)
+@bot.command(pass_context=True)
 async def task(ctx, task_name: str = None,):
+    """!task - lists all tasks not closed, if followed by task will give details for said task
+    
+    example:
+    !task will produce all tasks
+    !task "name" will produce info for said task eg; !task "task 11"
+    """
     if task_name is None:
         embed = discord.Embed(title="Task List", colour=discord.Colour(0x278d89))
         task_to_do = ''
@@ -61,8 +67,9 @@ async def task(ctx, task_name: str = None,):
             return
 
 
-@bot.command(pass_context=True, hidden=True)
+@bot.command(pass_context=True)
 async def closed(ctx):
+    """!closed - List all closed tasks"""
     embed = discord.Embed(title="Closed Task List", colour=discord.Colour(0x278d89))
     task_to_do = ''
     task_status = ''
@@ -83,8 +90,15 @@ async def closed(ctx):
     return
 
 
-@bot.command(pass_context=True, hidden=True)
+@bot.command(pass_context=True)
 async def addtask(ctx, task_name: str = None, task_description: str = None, status: str = None, assigned: str = None):
+    """!addtask <taskname> <task description> <status> <assigned>
+    all variables need to be wrapped in quotes eg !addtask "task name here" "task description" "status" "assigned to"
+    assigned to needs to be a string, not a mention eg; "probsjustin"
+    status must be, open, closed, or need more info.
+    example:
+    !addtask "finish website" "finish the about page" "open" "probsjustin"
+    """
     if not task_name or not task_description or not status or not assigned:
         await bot.say("Incorrect format, <addtask> <task name> <task description> <status> <assigned>")
 
@@ -92,7 +106,6 @@ async def addtask(ctx, task_name: str = None, task_description: str = None, stat
     if status.lower() not in status_choices:
         await bot.say("Valid status is, closed, open, or need more info.")
         return
-
 
     if task_name in tasks:
         await bot.say(f"Already a task named '{task_name}' please change the name or remove the old one.")
@@ -103,19 +116,25 @@ async def addtask(ctx, task_name: str = None, task_description: str = None, stat
     config.save()
 
 
-@bot.command(pass_context=True, hidden=True)
+@bot.command(pass_context=True)
 async def close(ctx, task: str = None):
+    """!close "task" - closes a task thats not already closed."""
     if not task:
         await bot.say('No task supplied!')
         return
 
     if task in tasks:
         get_status = tasks.get(task).get('status')
+        if get_status == "closed":
+            await bot.say(f"'{task}' is already closed.")
+            return
+        
         config.data['tasklist'][f'{task}'][f'{get_status}'] = "closed"
         config.save()
         await bot.say(f"Closed '{task}'")
     else:
         await bot.say(f"I couldn\'t find task '{task}'")
         return
+
 
 bot.loop.run_until_complete(connect())
